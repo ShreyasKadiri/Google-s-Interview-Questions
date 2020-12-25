a76b7018b276db930516cb87fabfb8c0d7b54868
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-12-25 13:07:06 +0530

    Update Single Number II.java

--- Single Number II.java
+++ Single Number II.java
@@ -13,3 +13,46 @@ class Solution {
         return -1;
     }
 }
+Time Complexity : O(n)
+Space Complexity: O(n)
+
+//Sorting 
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-3; i+=3){
+            if(nums[i]!=nums[i+2]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
+Time Complexity : O(nlogn)
+Space Complexity: O(1)
+    
+    
+//Bitwise  (Left shifting << means multiplying by 2)
+class Solution {
+    public int singleNumber(int[] nums) {
+        int shift = 1;
+        int result=0;
+        for(int i=0; i<32; i++){
+            int count=0;
+            for(int j=0; j<nums.length; j++){
+                if((nums[j] & shift)==1){
+                    count+=1;
+                }
+            }
+                if(count%3!=0)
+                    result+=shift;
+                    shift*=2;
+        }
+        return result;
+    }
+}    
+    
+    
+    
+    
+    
