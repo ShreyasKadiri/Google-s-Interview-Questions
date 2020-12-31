77cd8880582c50a96738d7b515dc4123051a2451
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2020-12-31 22:56:12 +0530

    Create survive in island.java

---
+++ survive in island.java
@@ -0,0 +1,11 @@
+class Solution { 
+	public static boolean survival(int S, int N, int M) { 
+		if (((N*6) < (M*7) && S>6) || M>N) 
+			return false;
+		else {  
+			int days = (M*S) / N; 
+			if (((M*S) % N)!=0) 
+				days++; 
+			return true;
+		} 
+	} 
