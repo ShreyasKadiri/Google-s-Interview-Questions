4d78dfd0e66fd844cb7c3f44409f3e3011cf3e77
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-01-24 14:59:32 +0530

    patching array greedy

---
+++ Patching array.java
@@ -0,0 +1,22 @@
+/*Approach
+https://leetcode.com/problems/patching-array/discuss/78488/Solution-%2B-explanation
+
+*/
+
+class Solution {
+    public int minPatches(int[] nums, int n) {
+    long missing=1;
+    int patches=0;
+    int i=0;
+
+    while (missing<=n) {
+        if (i<nums.length && nums[i]<=missing) {
+            missing+=nums[i++];
+        } else {
+            missing+=missing;
+            patches++;
+        }
+    }
+    return patches;
+}
+}
