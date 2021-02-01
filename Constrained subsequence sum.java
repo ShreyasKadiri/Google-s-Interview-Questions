c2880046295a46539c9b33b9606a8b844df265a6
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-01 14:19:32 +0530

    constrained subsequence sum using deque

--- Constrained subsequence sum.java
+++ Constrained subsequence sum.java
@@ -5,8 +5,12 @@ Genrate all possible subsets and chek for the sum with given condition
 Approach2:
 DP
 
+Approach 3:
+Deque
 */
 
+
+//DP Approach
 class Solution {
     public int constrainedSubsetSum(int[] nums, int k) {
         int dp[] =  new int[nums.length];
@@ -20,4 +24,35 @@ class Solution {
         }
         return result;
     }
+}
+
+
+
+//Deque Approach
+class Solution{
+    public int constrainedSubsetSum(int[] nums, int k) {
+        int dp[] = new int[nums.length];
+        int max = Integer.MIN_VALUE;
+        dp[0] = nums[0];
+        max = Math.max(dp[0], max);
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        deque.offer(nums[0]);
+        
+        for(int i=1; i<nums.length; i++){
+            dp[i] = nums[i];
+            
+            //If the element goes out of range
+            if(i>k && deque.peekFirst() == dp[i-k-1]){
+                deque.removeFirst();
+            }
+            dp[i] =  Math.max(dp[i], nums[i]+deque.peek());
+            
+            while(!deque.isEmpty() && deque.peekLast()<dp[i]){
+                deque.pollLast();
+            }
+            deque.offerLast(dp[i]);
+            max =  Math.max(dp[i],max);
+        }
+        return max;
+    }    
 }
