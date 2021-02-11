e81447b89748f858f8598a6bf937fa2a15b149aa
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-11 18:55:56 +0530

    bug fixed

--- Length of last word.java
+++ Length of last word.java
@@ -1,16 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        if (s == null || s.length() == 0)
+        if (s==null || s.length()==0)
             return 0;
-        int sizeOfLastWord = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ')
-                sizeOfLastWord++;
-            else if (sizeOfLastWord == 0) 
+        
+        int maxLastWordSize = 0;
+        for (int i=s.length()-1; i>=0; i--) {
+            if (s.charAt(i)!= ' '){
+                maxLastWordSize+=1;
+            }
+            else if (maxLastWordSize==0){
                 continue;
-            else 
+            } 
+            else {
                 break;
         }
-        return sizeOfLastWord;
     }
+    return maxLastWordSize;
+  }
 }
