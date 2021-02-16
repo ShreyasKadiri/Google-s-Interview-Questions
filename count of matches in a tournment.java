5e02b40ee2090880a7adf821661aee91909834a3
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-02-16 22:24:38 +0530

    Update count of matches in a tournment.java

--- count of matches in a tournment.java
+++ count of matches in a tournment.java
@@ -1,16 +1,5 @@
 class Solution {
     public int numberOfMatches(int n) {
-    int matchesPlayed=0;
-    int matches=0;
-    int teamsAdvance=0;
-    int temp=n;
-        while(n>=1){
-            matches=(int)Math.floor(n/2);
-            matchesPlayed+=matches;
-            teamsAdvance = temp - matchesPlayed;
-            temp = teamsAdvance;
-        }
-        
-        return matchesPlayed;
+        return n-1;
     }
 }
