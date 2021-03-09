b447487902c5c6f30f4a88a2dcafbc39034f616b
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-03-09 18:04:33 +0530

    optimal added

--- Prime Number of Set Bits in Binary Representation.java
+++ Prime Number of Set Bits in Binary Representation.java
@@ -1,3 +1,4 @@
+//Naive --- O(n3)
 class Solution {
     public int countPrimeSetBits(int L, int R) {
         int count = 0;
@@ -25,5 +26,25 @@ class Solution {
                 return false; 
   
         return true;   
+    }
+}
+
+
+
+
+
+class Solution{    
+    private boolean isPrime (int n) {
+        return n==2 || n==3 || n==5 || n==7 || n==11 || n==13 || n==17 || n==19;
+    }
+    
+    public int countPrimeSetBits(int L, int R) {
+        int result = 0;
+        while (L<=R) {
+            if (isPrime(Integer.bitCount (L++))) {
+                ++result;
+            }
+        }
+        return result;
     }
 }
