8e8db1ce2bd2a29f4f0432c0151398be83844a8d
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-03 12:15:34 +0530

    comments added

--- Minimum Numbers of Function Calls to Make Target Array.java
+++ Minimum Numbers of Function Calls to Make Target Array.java
@@ -4,6 +4,8 @@ class Solution {
         while(true){
             int zeroCount = 0 ;
             int i = 0;
+            
+            //Keep track of zeroes
             for(i=0 ; i<nums.length; i++){
                 if( (nums[i]&1) == 1) {
                     break;
@@ -11,22 +13,27 @@ class Solution {
                      zeroCount+=1;
                 }
             }
+            
+            //Once all the array elements are made zeroes , then just return count
             if(zeroCount  == nums.length){
                  return count;
             }
             
+            
+            //If even, then divide by 2 and hence increment count
             if(i == nums.length){ 
                 count+=1; 
                 for(i=0 ; i<nums.length; i++) 
                     nums[i]/=2;
             }
+            
+            //If odd, then subtract 1 from number to make it even and hence increment count
             for(i=0 ; i<nums.length; i++){
                 if((nums[i]&1) == 1){
                     count+=1;
                     nums[i]-=1;
                 }
             }
-            
         }
     }
 }
