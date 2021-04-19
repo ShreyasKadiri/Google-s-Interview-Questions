32348f1a13cb7fd6649964c3f9c1d64d74010375
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-19 18:37:51 +0530

    template added

--- combinations iv.java
+++ combinations iv.java
@@ -1,4 +1,21 @@
 class Solution { 
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int i=0;  i<coins.length; i++) {
+            for (int j=1; j<=amount; j++) {
+                if (j >= coins[i]) {
+                    dp[j] += dp[j - coins[i]];    
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
+
+
+//Combinations Sum IV
+class Solution { 
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target+1];
         dp[0] = 1;
