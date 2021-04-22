096e474462e8cf6a3c531ff51e4447eadac7e3ca
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-04-22 18:47:18 +0530

    finding sum when 0

--- brick wall.java
+++ brick wall.java
@@ -1,17 +1,24 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         int result = Integer.MAX_VALUE;
+        int sum = 0;
         HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
         for(int i=0; i<wall.size(); i++){
             int position = 0;
             for(int j=0; j<wall.get(i).size(); j++){
                 position+= wall.get(i).get(j);
                 map.put(position, map.getOrDefault(position,0)+1);
+                if(i == 0){
+                    sum+= wall.get(i).get(j);
+                }
             }
         }
         for(Integer key : map.keySet()){
+            if(sum == key){
+                continue;
+            }
             result = Math.min(result, wall.size() - map.get(key));
         }
-        return  result == Integer.MA_VALUE ? wall.size(): result;
+        return  result == Integer.MAX_VALUE ? wall.size(): result;
     }
 }
