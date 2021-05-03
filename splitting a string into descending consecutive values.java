64a72cc60ea1a478b466eab973eb742acbdb969a
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-03 17:24:41 +0530

    backtracking

---
+++ splitting a string into descending consecutive values.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean splitString(String s) {
+        if (s == null || s.length()<=1){
+            return false;
+        }
+        return backtrack(0, s, new ArrayList<Long>());
+    }
+    
+    public boolean backtrack(int position, String s, ArrayList<Long> result) {
+        if (position >= s.length()) {
+            return result.size() >= 2;
+        }
+
+        long value = 0;
+        for (int i=position; i<s.length(); i++) {
+            value = value*10 + (s.charAt(i)-'0'); 
+            if (result.size()==0 || result.get(result.size()-1) - num == 1) {
+                result.add(num); 
+                if (backtrack(i+1, s, result)) {
+                    return true;
+                }
+                result.remove(list.size()-1); 
+            }
+        }
+        return false;
+    }
+}
