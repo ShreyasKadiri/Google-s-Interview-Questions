385dad709079e17a2b73455adf333550b316f3a7
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-14 13:43:32 +0530

    recursive + stack + iterative

--- flatten a binary tree to linkedlist.java
+++ flatten a binary tree to linkedlist.java
@@ -67,3 +67,36 @@ class Solution {
         }
     }
 }
+
+//Iterative
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode currentNode = root;
+        while (currentNode != null){
+            if (currentNode.left != null){
+                TreeNode previousNode = currentNode.left;
+                while (previousNode.right != null)
+                    previousNode = previousNode.right;
+                    previousNode.right = currentNode.right;
+                    currentNode.right = currentNode.left;
+                    currentNode.left = null;
+            }
+            currentNode = currentNode.right;
+        }
+    }
+}
