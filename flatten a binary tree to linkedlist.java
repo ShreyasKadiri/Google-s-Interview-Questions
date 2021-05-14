f4607bd571d97a4ea08a3d26c94a4f498742edb4
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2021-05-14 13:27:29 +0530

    stack approach updated

--- flatten a binary tree to linkedlist.java
+++ flatten a binary tree to linkedlist.java
@@ -26,3 +26,44 @@ class Solution {
         previousNode = root;
     }
 }
+
+
+//Stack
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode currentNode = stack.peek();
+            stack.pop();
+            if (currentNode.right != null){
+                stack.push(currentNode.right);
+            }
+            if (currentNode.left != null){
+                stack.push(currentNode.left);
+            }
+            if (!stack.isEmpty()){
+                currentNode.right = stack.peek();
+            }
+            currentNode.left = null;
+        }
+    }
+}
